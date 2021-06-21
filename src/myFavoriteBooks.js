a80d0f6978b2b8734d7459a5a3368bd2f4f30c44
anas <civil.anas1000@gmail.com>
2021-06-21 23:44:15 +0300

    fix herouko

--- src/myFavoriteBooks.js
+++ src/myFavoriteBooks.js
@@ -1,30 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./myFavoriteBooks.css";
-import Carousel from "react-bootstrap/Carousel";
 import BestBooks from "./Component/BestBooks";
 
 class MyFavoriteBooks extends React.Component {
   render() {
     return (
-      // <Carousel>
-      //   {this.props.dataBooks.map((item) => {
-      //     return (
-           
-      //       <Carousel.Item>
-      //         <img
-      //           className="d-block w-100"
-      //           src={`${item.url} slide&bg=373940`}
-      //           alt="First slide"
-      //         />
-      //         <Carousel.Caption>
-      //           <h3>{item.name}</h3>
-      //           <p>{item.description} </p>
-      //         </Carousel.Caption>
-      //       </Carousel.Item>
-      //     );
-      //   })}
-      // </Carousel>
+
       <BestBooks dataBooks={this.props.dataBooks}/>
     );
   }
